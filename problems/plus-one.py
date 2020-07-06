0491c062b755ff46e16730105efb0984156d6aee
Mark W Girard <mark.w.girard@gmail.com>
2020-07-06 09:45:51 -0400

    Update plus-one.py

--- problems/plus-one.py
+++ problems/plus-one.py
@@ -1,6 +1,9 @@
 class Solution:
     # The fast solution.
     #def plusOne(self, digits: List[int]) -> List[int]:
+    ```
+    Stops at the first digit that is not a 9 and adds 1. Otherwise, turn the 9's into 0's. If every digit turns into a zero, return [1,0,...,0].
+    ```
     #    i = len(digits) - 1
     #    while digits[i] == 9:
     #        digits[i] = 0
