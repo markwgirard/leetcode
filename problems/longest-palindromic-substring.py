5429b5430f692ea58ebb7df6ec2ca95c5ad3181c
Mark W Girard <mark.w.girard@gmail.com>
2020-07-07 09:12:11 -0400

    Update longest-palindromic-substring.py

--- problems/longest-palindromic-substring.py
+++ problems/longest-palindromic-substring.py
@@ -15,3 +15,19 @@ class Solution:
             if len(palindrome) > len(max_palindrome):
                 max_palindrome = palindrome
         return max_palindrome
+    
+# Faster solution  
+class Solution:
+    def longestPalindrome(self, s):
+        if not s: return ''
+        maxLen=1
+        start=0
+        for i in range(len(s)):
+        	if i-maxLen >=1 and s[i-maxLen-1:i+1]==s[i-maxLen-1:i+1][::-1]:
+        		start=i-maxLen-1
+        		maxLen+=2
+        		continue
+        	if i-maxLen >=0 and s[i-maxLen:i+1]==s[i-maxLen:i+1][::-1]:
+        		start=i-maxLen
+        		maxLen+=1
+        return s[start:start+maxLen]
