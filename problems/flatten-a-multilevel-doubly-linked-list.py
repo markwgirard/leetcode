68ba5a4d287ce6b63a8982a13d741b87ea4b2e83
Mark W Girard <mark.w.girard@gmail.com>
2020-07-10 09:43:56 -0400

    Update flatten-a-multilevel-doubly-linked-list.py

--- problems/flatten-a-multilevel-doubly-linked-list.py
+++ problems/flatten-a-multilevel-doubly-linked-list.py
@@ -12,3 +12,15 @@ class Solution:
             node.child = None
             prevNode = node
         return head
+    
+# more compact solution
+class Solution:
+    def flatten(self, head: 'Node') -> 'Node':
+        if not head: return None
+        stack, prevNode = [head], Node()
+        while stack:
+            root = stack.pop()
+            stack.extend([node for node in (root.next, root.child) if node])
+            root.prev, root.child, prevNode.next, prevNode = prevNode, None, root, root
+        head.prev = None
+        return head
