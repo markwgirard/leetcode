057d0d77b725dd49ebc73694c2d7e0f1b9a0c698
Mark W Girard <mark.w.girard@gmail.com>
2020-07-17 09:28:02 -0400

    Update top-k-frequent-elements.py

--- problems/top-k-frequent-elements.py
+++ problems/top-k-frequent-elements.py
@@ -10,3 +10,18 @@ class Solution:
         hp = [(-freqs[num], num) for num in freqs]
         heapq.heapify(hp)
         return [heapq.heappop(hp)[1] for _ in range(k)]
+    
+    
+    
+# Bucket sort: O(n) time and O(n) space
+
+from collections import Counter
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        buckets = [[] for _ in range(len(nums) + 1)]
+        count = Counter(nums).items()  
+        for num, freq in count: 
+            buckets[freq].append(num) 
+        flat_list = [item for bucket in buckets for item in bucket]
+        return flat_list[::-1][:k]
