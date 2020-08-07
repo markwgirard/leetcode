3fb9d4b109a301c1a4d4d35ef9f3802e3248ae00
Mark W Girard <mark.w.girard@gmail.com>
2020-08-07 08:36:15 -0400

    Update valid-palindrome.py

--- problems/valid-palindrome.py
+++ problems/valid-palindrome.py
@@ -1,4 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        a = (''.join([c for c in s if c.isalnum()])).lower()
-        return a == a[::-1]
+        # one-liner
+        #return (lambda a: a==a[::-1])((''.join([c for c in s if c.isalnum()])).lower())
+        
+        # O(1) space 
+        l, r = 0, len(s) - 1
+        while l < r:
+            if not s[l].isalnum(): l+=1
+            elif not s[r].isalnum(): r-=1
+            else:
+                if s[l].lower()==s[r].lower():
+                    l, r = l+1, r-1
+                else:
+                    return False
+        return True
